9314dab5e8575f85083908fe1908fdca9c905125
walter <wwan999@aucklanduni.ac.nz>
2020-09-08 14:58:35 +1200

    pagination

--- public/js/pagebar.js
+++ public/js/pagebar.js
@@ -1,4 +1,10 @@
-let articlesCount,pageCount,currentPage;
+let articlesCount, pageCount, currentPage;
+
+
+getCount();
+totalPage(articlesCount);
+getCurrentPage();
+generatePagination();
 
 function getCount() {
     let countDiv = document.querySelector('.count');
@@ -6,33 +12,26 @@ function getCount() {
     countDiv.remove();
 }
 
-getCount();
-
-
-function totalPage (count) {
+function totalPage(count) {
     pageCount = Math.ceil(count / 10);
 }
-
-totalPage(articlesCount);
 
 function getCurrentPage() {
     let currentPageDiv = document.querySelector('.currentPage');
     currentPage = parseInt(currentPageDiv.innerHTML);
-    console.log(currentPage);
 }
 
-getCurrentPage();
-generatePagination();
-
-function generatePagination () {
+function generatePagination() {
     let ul = document.querySelector('.pagination');
     createPrevious(ul);
+    createPage(ul);
     createNext(ul);
 }
 
-function createLi (href,content) {
+function createLi(href, content) {
     let li = document.createElement('li');
     li.classList.add("page-item");
+    li.classList.add("w-50");
     let a = document.createElement('a');
     a.classList.add("page-link");
     a.href = href;
@@ -41,8 +40,8 @@ function createLi (href,content) {
     return li;
 }
 
-function createPrevious (ul) {
-    let previous,href,content;
+function createPrevious(ul) {
+    let previous, href, content;
     content = "previous";
     href = `/page/${currentPage - 1}`;
     previous = createLi(href, content);
@@ -52,13 +51,41 @@ function createPrevious (ul) {
     ul.appendChild(previous);
 }
 
-function createNext (ul) {
-    let next,href,content;
+function createNext(ul) {
+    let next, href, content;
     content = "next";
     href = `/page/${currentPage + 1}`;
     next = createLi(href, content);
-    if (currentPage == totalPage) {
-        previous.classList.add('disabled');
+    if (currentPage == pageCount) {
+        next.classList.add('disabled');
     }
     ul.appendChild(next);
+}
+
+function createPage(ul) {
+    let page, href, content, startPoint, endPoint;
+
+    if (pageCount <= 10 || currentPage <= 5) {
+        startPoint = 1;
+        endPoint = Math.min(pageCount,10);
+    } else if (pageCount - currentPage <= 5) {
+        startPoint = pageCount - 9;
+        endPoint = pageCount;
+    } else {
+        startPoint = currentPage - 4;
+        endPoint = currentPage + 5;
+    }
+
+    for (let i = startPoint; i <= endPoint; i++) {
+        content = `${i}`;
+        href = `/page/${i}`;
+        page = createLi(href, content);
+        if (i == currentPage) {
+            page.classList.add("active");
+        }
+        ul.appendChild(page);
+    }
+
+
+
 }
--- routers/publish.js
+++ routers/publish.js
@@ -12,9 +12,9 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    
+
     let id = Date.now();
-    let date = format(id,'yyyy-mm-dd HH:MM:ss');
+    let date = format(id, 'yyyy-mm-dd HH:MM:ss');
     let data = {
         username: req.session.username,
         title: req.body.title,
@@ -31,8 +31,25 @@ router.post('/', (req, res) => {
         }
         res.redirect('/');
     });
-    
+
 
 })
+
+function add200Ariticle() {
+    for (let i = 0; i < 200; i++) {
+        let id = Date.now();
+        let date = format(id, 'yyyy-mm-dd HH:MM:ss');
+        let data = {
+            username: `walter`,
+            title: `Lorem Ipsum${i}`,
+            content: `Where does it come from?
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+            date: date,
+            id: id
+        }
+        let newArticle = new ArticleModel(data);
+        newArticle.save();
+    }
+}
 
 module.exports = router;
